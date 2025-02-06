2ee06a256d27046f9187b5dcfc89555b6855f05e
chuchu04 <118867760+chuchu04@users.noreply.github.com>
2025-02-06 02:34:21 +0000

    delete cmt

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import "./NavBar.css"
 import logo_light from "../assets/logo-black.png"
 import logo_dark from "../assets/logo-white.png"
 import search_icon_light from "../assets/search-w.png"
-import search_icon_dark from "../assets/search-b.png" ///test
+import search_icon_dark from "../assets/search-b.png"
 import toggle_light from "../assets/night.png"
 import toggle_dark from "../assets/day.png"
 const NavBar = ({theme, setTheme}) =>{
