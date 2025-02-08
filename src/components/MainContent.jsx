63ff06df981e4f5a4d520efbf56351f442711703
blacki0214 <84124135+blacki0214@users.noreply.github.com>
2025-02-08 04:04:18 +0000

    enhance main content

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import "./MainContent.css";
+import search_icon_light from "../assets/search-w.png";
+import search_icon_dark from "../assets/search-b.png";
 
-const MainContent = () =>{
+const MainContent = ({theme, setTheme}) =>{
+    const toggle_mode = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    }
+
     return(
         <div className="content">
             <h1>Real-time Tracking with ABCXYZ</h1>
 
             <div className="search-trans">
                 <input type="text" placeholder="Search Transactions"/>
-
+                <img 
+                    src={theme === 'light' ? search_icon_light : search_icon_dark} 
+                    alt="Search" 
+                    className="search-icon"
+                />
             </div>     
         </div>
 
