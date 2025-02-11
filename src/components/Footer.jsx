97ce6eea9a25b9aaaff44f3986a1b059a840b6ec
chuchu04 <118867760+chuchu04@users.noreply.github.com>
2025-02-11 18:02:33 +0000

    update footer icon + css (done)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./Footer.css";
-
+import { FaFacebookF, FaDiscord, FaTwitter, FaTh } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <footer>
             <div className="footer-content">
                 <ul className="footer-icons">
-                    <li><a href="#" aria-label="Facebook"><i className="facebook-icon">Facebook</i></a></li>
-                    <li><a href="#" aria-label="Discord"><i className="discord-icon">Discord</i></a></li>
-                    <li><a href="#" aria-label="Twitter"><i className="twitter-icon">Twitter</i></a></li>
-                    <li><a href="#" aria-label="Threads"><i className="threads-icon">Threads</i></a></li>
+                    <li><a href="#" aria-label="Facebook"><FaFacebookF className="facebook-icon" /> Facebook</a></li>
+                    <li><a href="#" aria-label="Discord"><FaDiscord className="discord-icon" /> Discord</a></li>
+                    <li><a href="#" aria-label="Twitter"><FaTwitter className="twitter-icon" /> Twitter</a></li>
+                    <li><a href="#" aria-label="Threads"><FaTh className="threads-icon" /> Threads</a></li>
                 </ul>
                 <div className="footer-text">
                     <span className="copyright">© 2025 ABCXYZ. All Rights Reserved.</span>
